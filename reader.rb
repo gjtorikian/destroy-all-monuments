725a096561e5e7fae2f121416a2dc8bf6c042dab
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2017-08-17 01:05:03 -0400

    More cleanup

--- reader.rb
+++ reader.rb
@@ -1,6 +1,13 @@
 require 'smarter_csv'
+require 'awesome_print'
 
 Dir.glob('csv/*.csv').each do |csv|
   puts csv
-  SmarterCSV.process(csv)
+  chunk = SmarterCSV.process(csv)
+  chunk.each do |data|
+    if data.keys.count != 4
+      ap "UGH bad data #{data}"
+    end
+  end
+  puts chunk.size
 end
